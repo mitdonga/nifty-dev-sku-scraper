5643a52ef72052259f63b44f134035292f3b175c
Mit Donga <meetdonga5344@gmail.com>
2025-11-27 12:12:26 +0530

    Quick UI fix

--- src/components/SKUDetailDrawer.jsx
+++ src/components/SKUDetailDrawer.jsx
@@ -42,7 +42,7 @@ function SKUDetailDrawer({ isOpen, onClose, skuData }) {
 
       {/* Drawer */}
       <div
-        className={`fixed top-0 left-0 h-full w-[70%] bg-white shadow-2xl z-50 transform transition-transform duration-300 ease-in-out ${
+        className={`fixed top-0 left-0 h-full w-full md:w-[70%] bg-white shadow-2xl z-50 transform transition-transform duration-300 ease-in-out ${
           isOpen ? 'translate-x-0' : '-translate-x-full'
         } overflow-y-auto`}
       >
